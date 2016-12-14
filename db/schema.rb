9811d6e6393451958490bd2e8df53c9d9dcd5f9e
Bruno De Wolf <bruno@levantus.be>
2016-12-14 21:17:22 +0100

    Revert "VAT number of companies"
    This reverts commit 8a7bb947fb6dff585939cde33781215295518856.

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 20160817124148) do
 ActiveRecord::Schema.define(version: 20161212212133) do
 
   create_table "account_users", force: true do |t|
